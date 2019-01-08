1c5fe9c286494f37664048aab96c2b0085c1803e
Saksham Dhull <sakshamdhull442@gmail.com>
2019-01-08 17:22:08 +0530

    airplane data

---
+++ airplane/conv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+a=$(ls | grep "airline_orange")
+lim=$(echo $a | wc -w)
+i=1
+while [[ i -le lim ]]; do
+	#statements
+	name=$(echo $a | cut -f $i -d' ')
+	newname=$(echo $name| cut -f1 -d'.')
+	mv "$name" "$newname.png"
+	((i++))
+done
